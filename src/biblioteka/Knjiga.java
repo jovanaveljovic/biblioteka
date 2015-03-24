43b357176dee8ce9c18786fd59fcfac18f67b801
jovanaveljovic <jovanaveljovic99@yahoo.com>
2015-03-24 09:32:45 +0100

    Refaktorisana klasa Knjiga, uvedena klasa Autor

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,22 +1,26 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
 
 	private String naslov;
-	private String autori;
 	private String izdavac;
 	private long ISBN;
 	private int izdanje;
+	private LinkedList<Autor> autori = new LinkedList<Autor>();
+	
 	public String getNaslov() {
 		return naslov;
 	}
 	public void setNaslov(String naslov) {
 		this.naslov = naslov;
 	}
-	public String getAutori() {
+	
+	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
-	public void setAutori(String autori) {
+	public void setAutori(LinkedList<Autor> autori) {
 		this.autori = autori;
 	}
 	public String getIzdavac() {
